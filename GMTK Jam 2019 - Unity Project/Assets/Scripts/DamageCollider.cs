18eb5e4d7ea6b265658e8d30218b381ae433e6ad
fr4ct1ons <42714282+fr4ct1ons@users.noreply.github.com>
2019-08-03 17:45:23 -0300

    Added basic punch attack

---
+++ GMTK Jam 2019 - Unity Project/Assets/Scripts/DamageCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCollider : MonoBehaviour
+{
+    [SerializeField] float damageToDeal, stun;
+    [SerializeField] Vector3 knockback;
+    [SerializeField] GameObject owner;
+
+    Vector3 bufferVector = new Vector3(0, 0, 0);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.Equals(owner))
+            DealDamage(other.gameObject);
+    }
+
+    public void DealDamage(GameObject target)
+    {
+        target.GetComponent<PlayerController>().AddDamage(damageToDeal, stun);
+        bufferVector = knockback;
+        bufferVector.x *= owner.GetComponent<PlayerController>().GetBodyRotation();
+        target.GetComponent<Rigidbody>().AddForce(bufferVector * target.GetComponent<PlayerController>().GetDamage());
+    }
+}
--- GMTK Jam 2019 - Unity Project/Assets/Scripts/PlayerController.cs
+++ GMTK Jam 2019 - Unity Project/Assets/Scripts/PlayerController.cs
@@ -6,39 +6,59 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] string controllerNumber = "0";
     [SerializeField] int speed;
-
+    
     Vector3 bufferVector;
     Rigidbody myRigidbody;
+    Animator myAnimator;
     int bodyRotation = 1;
+    [SerializeField] float damagePercentage = 0;
+    bool canMove = true;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
+        myAnimator = GetComponent<Animator>();
         bufferVector = transform.position;
+
+        if (transform.eulerAngles.y >= 85.0f && transform.eulerAngles.y <= 95.0f)
+        {
+            bodyRotation = 1;
+        }
+        else if (transform.eulerAngles.y >= 265 && transform.eulerAngles.y <= 275)
+        {
+            bodyRotation = -1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         //cPrint(Input.GetAxisRaw("Joy" + controllerNumber + "Horizontal"));
-
-        if (GetAxisUni("Joy" + controllerNumber + "Horizontal") != bodyRotation && GetAxisUni("Joy" + controllerNumber + "Horizontal") != 0)
+        if (canMove)
         {
-            cPrint("Input is " + GetAxisUni("Joy" + controllerNumber + "Horizontal") + " bodyRotation is" + bodyRotation);
-            cPrint("Rotating.");
-            transform.Rotate(0.0f, 180.0f, 0.0f);
-        }
+            if (GetAxisUni("Joy" + controllerNumber + "Horizontal") != bodyRotation && GetAxisUni("Joy" + controllerNumber + "Horizontal") != 0)
+            {
+                cPrint("Input is " + GetAxisUni("Joy" + controllerNumber + "Horizontal") + " bodyRotation is" + bodyRotation);
+                cPrint("Rotating.");
+                transform.Rotate(0.0f, 180.0f, 0.0f);
+            }
 
-        if (transform.eulerAngles.y >= 85.0f && transform.eulerAngles.y <= 95.0f)
-        {
-            bodyRotation = 1;
-        }
-        else if (transform.eulerAngles.y >= 265 && transform.eulerAngles.y <= 275 )
-        {
-            bodyRotation = -1;
-        }
+            if (transform.eulerAngles.y >= 85.0f && transform.eulerAngles.y <= 95.0f)
+            {
+                bodyRotation = 1;
+            }
+            else if (transform.eulerAngles.y >= 265 && transform.eulerAngles.y <= 275)
+            {
+                bodyRotation = -1;
+            }
 
-        myRigidbody.MovePosition(transform.position + (transform.forward * GetAxisUni("Joy" + controllerNumber + "Horizontal") * speed * Time.deltaTime * bodyRotation));
+            myRigidbody.MovePosition(transform.position + (transform.forward * GetAxisUni("Joy" + controllerNumber + "Horizontal") * speed * Time.deltaTime * bodyRotation));
+            if (Input.GetButton("Joy" + controllerNumber + "BasicPunch"))
+            {
+                myAnimator.SetTrigger("BasicPunch");
+                cPrint("Basic Punch");
+            }
+        }
     }
 
     void cPrint(object mymessage)
@@ -58,4 +78,25 @@ public class PlayerController : MonoBehaviour
         else
             return 0;
     }
+
+    public void AddDamage(float value, float stun)
+    {
+        damagePercentage += value;
+        if(stun != 0.0f)
+        {
+            StartCoroutine(Stun(stun));
+        }
+    }
+
+    private IEnumerator Stun(float time)
+    {
+        canMove = false;
+        yield return new WaitForSeconds(time);
+        canMove = true;
+    }
+
+    public float GetDamage() { return damagePercentage; }
+    public int GetBodyRotation() { return bodyRotation; }
+    public void UnallowMovement() { canMove = false; }
+    public void AllowMovement() { canMove = true; }
 }
